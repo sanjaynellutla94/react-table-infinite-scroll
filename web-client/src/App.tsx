dbb63b77e41511d243c5110822b39b5be1887418
sanjaynellutla94 <nellutlasanjay@gmail.com>
2020-12-24 13:15:57 +0530

    Added useCallbacks and useMemos at required places.

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { getTableDataFact, getXHRErrorMeta } from './api/api';
 import { Table } from './components';
 
-function App() {
+const App = () => {
   const [columns, setColumns] = useState([]);
   const [rows, setRows] = useState([]);
   const [meta, setMeta] = useState({
@@ -17,22 +17,37 @@ function App() {
     totalPassengers: 0,
   });
 
+  // This hook should only be called on component init
   useEffect(() => {
     generateTableColumns();
     getTableData(meta.page, meta.size);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const generateTableColumns = () => {
-    const data: any = [
-      { label: 'Name', key: 'name', hasSort: true },
-      { label: 'Trips', key: 'trips', hasSort: false },
-      { label: 'Airline', key: 'airline', hasSort: false, renderItem: () => <div className="text-primary">Airline</div> },
-      { label: 'Country', key: 'country', hasSort: false },
-      { label: 'Actions', key: 'actions', hasSort: false },
-    ]
-    setColumns(data);
-  };
+  // Using useCallback because functions are dependent on light state.
+
+  const generateTableColumns = useCallback(
+    () => {
+      const data: any = [
+        { label: 'Name', key: 'name', hasSort: true },
+        { label: 'Trips', key: 'trips', hasSort: false },
+        { label: 'Airline', key: 'airline', hasSort: false, renderItem: () => <div className="text-primary">Airline</div> },
+        { label: 'Country', key: 'country', hasSort: false },
+        { label: 'Actions', key: 'actions', hasSort: false },
+      ]
+      setColumns(data);
+    },
+    [setColumns],
+  );
+
+  const getnerateMetadata = useCallback((data: any) => {
+    setMeta({
+      ...meta,
+      ...data,
+    });
+  }, [setMeta, meta]);
+
+  // Not using useCallback because functions are dependent on heavy state and comparision is costly.
 
   const generateTableRows = (data: any) => {
     const mapped: any = data.map((item: any) => {
@@ -45,20 +60,6 @@ function App() {
     });
     const rowsData: any = [...rows, ...mapped];
     setRows(rowsData);
-  };
-
-  const getnerateMetadata = (data: any) => {
-    setMeta({
-      ...meta,
-      ...data,
-    });
-  };
-
-  const onInfinite = () => {
-    if (!rows.length) return;
-    const newPage = meta.page + 1;
-    if (newPage > meta.totalPages) return;
-    getTableData(newPage, meta.size);
   };
 
   const getTableData = (page: number, size: number) => {
@@ -92,17 +93,45 @@ function App() {
     })
   };
 
+  const onInfinite = () => {
+    if (!rows.length) return;
+    const newPage = meta.page + 1;
+    if (newPage > meta.totalPages) return;
+    getTableData(newPage, meta.size);
+  };
+
+  // Template functions
+
+  // Using useCallback because function is not dependent on other state
+  const renderItem = useCallback((item: any) => {
+    return (
+      <>
+        <Table.Data>{item.name}</Table.Data>
+        <Table.Data>{item.trips}</Table.Data>
+        <Table.Data>{item.airline}</Table.Data>
+        <Table.Data>{item.country}</Table.Data>
+        <Table.Data>
+          {/* Writing direct button and related css for demo, in live project i will create a reusable button component with scoped scss.  */}
+          <button className="btn btn-danger btn-sm" onClick={() => {
+            onDeleteItem(item._id);
+          }} disabled>Remove</button>
+        </Table.Data>
+      </>
+    );
+  }, []);
+
+  // Actions
+
   const onDeleteItem = (id: string | number) => {
     // Make a api call to delete record and get last item of ${meta.page} page items as response or can call a seperate api.
   };
 
-  // const onAddItem = (id: string | number) => {
-    // Make a api call to add record.
-    // order or position on new record depends on sort/filters applied.
+  // const onAddItem = (data) => {
+  // better to clear complete rows and call fresh data since sort/filter or new item order will not be clear.
   // };
 
   // const onUpdateItem = (id, data) => {
-    // find item with id and replace in rows list
+  // find item with id and replace in rows list
   // }
 
   if (meta.err.msg) {
@@ -125,22 +154,7 @@ function App() {
             </div>}
           </div>
         }
-        render={(item: any) => {
-          return (
-            <>
-              <Table.Data>{item.name}</Table.Data>
-              <Table.Data>{item.trips}</Table.Data>
-              <Table.Data>{item.airline}</Table.Data>
-              <Table.Data>{item.country}</Table.Data>
-              <Table.Data>
-                {/* Writing direct button and related css for demo, in live project i will create a reusable button component with scoped scss.  */}
-                <button className="btn btn-danger btn-sm" onClick={() => {
-                  onDeleteItem(item._id);
-                }} disabled>Remove</button>
-              </Table.Data>
-            </>
-          );
-        }}
+        render={renderItem}
         onInfinite={onInfinite}
       >
       </Table>
--- web-client/src/components/Table/Table.tsx
+++ web-client/src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styles from "./Table.module.scss";
 import Text from "../Text/Text";
 import InfiniteScroll from '../InfiniteScroll';
@@ -36,18 +36,19 @@ interface TableDataProps { children?: React.ReactNode; dataClass?: string }
 
 // Helper functions
 
-const getTableClass = (props: TableProps) => {
-  const { variant, type, tableClass } = props;
-  const typeClass = styles[`table-${type}`];
-  const variantClass = styles[`table-${variant}`];
-  return `${styles["table"]} ${typeClass} ${variantClass} ${tableClass}`;
-};
 
 const getContainerClass = (props: TableProps) => {
   const responsiveClass = props.responsive
     ? styles[`table-responsive${props.breakPoint}`]
     : "";
   return `${responsiveClass} ${props.containerClass}`;
+};
+
+const getTableClass = (props: TableProps) => {
+  const { variant, type, tableClass } = props;
+  const typeClass = styles[`table-${type}`];
+  const variantClass = styles[`table-${variant}`];
+  return `${styles["table"]} ${typeClass} ${variantClass} ${tableClass}`;
 };
 
 const getHeaderClass = (props: TableProps) => {
@@ -59,20 +60,27 @@ const getBodyClass = (props: TableProps) => {
   return props.bodyClass;
 };
 
-const getLoadingTemplate = (props: TableProps) => {
-  const { hasInfiniteScroll, onInfinite, loadingContent } = props;
-  if (hasInfiniteScroll && onInfinite) {
-    return <tr>
-      <td colSpan={props.columns.length}>
-        <InfiniteScroll onChange={(inView: boolean) => {
-          if (inView) onInfinite(inView);
-        }}>
-          {loadingContent}
-        </InfiniteScroll>
-      </td>
-    </tr>
+// Template functions
+
+const getColumnTemplate = (item: any) => {
+  if (item.renderItem) {
+    return item.renderItem(item);
+  } else if (item.hasSort) {
+    return (
+      <div className="d-flex">
+        <Text.H3>{item.label}</Text.H3>
+        <div className="ml-auto">
+          {/* We Can include a icon library and use its icons */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sort-alpha-down-alt" viewBox="0 0 16 16">
+            <path d="M12.96 7H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V7z" />
+            <path fillRule="evenodd" d="M10.082 12.629L9.664 14H8.598l1.789-5.332h1.234L13.402 14h-1.12l-.419-1.371h-1.781zm1.57-.785L11 9.688h-.047l-.652 2.156h1.351z" />
+            <path d="M4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z" />
+          </svg>
+        </div>
+      </div>
+    );
   }
-  return loadingContent;
+  return <Text.H3>{item.label}</Text.H3>;
 };
 
 // Components
@@ -82,46 +90,51 @@ const TableData = (props: TableDataProps) => {
 };
 
 const Table = (props: TableProps) => {
+  // Not using useMemo for all class utils because there are no heavy computations involved
   const containerClass = getContainerClass(props);
   const tableClass = getTableClass(props);
   const headerClass = getHeaderClass(props);
   const bodyClass = getBodyClass(props);
+
+  const { hasInfiniteScroll, onInfinite, loadingContent, columns, rows, render } = props;
+
+  const columnsLength = columns.length;
+
+  // Template functions
+
+  // Using useMemo since InfiniteScroll component is involved.
+  const loadingTemplate = useMemo(() => {
+    if (hasInfiniteScroll && onInfinite) {
+      return <tr>
+        <td colSpan={columnsLength}>
+          <InfiniteScroll onChange={(inView: boolean) => {
+            if (inView) onInfinite(inView);
+          }}>
+            {loadingContent}
+          </InfiniteScroll>
+        </td>
+      </tr>
+    }
+    return loadingContent;
+  }, [hasInfiniteScroll, onInfinite, loadingContent, columnsLength]);
+
+  // Render
 
   return (
     <div className={containerClass}>
       <table className={tableClass}>
         <thead className={headerClass}>
           <tr>
-            {props.columns.map((item: any) => {
-              let content = null;
-              if (item.renderItem) {
-                content = item.renderItem(item);
-              } else if (item.hasSort) {
-                content = (
-                  <div className="d-flex">
-                    <Text.H3>{item.label}</Text.H3>
-                    <div className="ml-auto">
-                      {/* We Can include a icon library and use its icons */}
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sort-alpha-down-alt" viewBox="0 0 16 16">
-                        <path d="M12.96 7H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V7z" />
-                        <path fillRule="evenodd" d="M10.082 12.629L9.664 14H8.598l1.789-5.332h1.234L13.402 14h-1.12l-.419-1.371h-1.781zm1.57-.785L11 9.688h-.047l-.652 2.156h1.351z" />
-                        <path d="M4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z" />
-                      </svg>
-                    </div>
-                  </div>
-                );
-              } else {
-                content = <Text.H3>{item.label}</Text.H3>;
-              }
-              return <th key={item.key}>{content}</th>;
+            {columns.map((item: any) => {
+              return <th key={item.key}>{getColumnTemplate(item)}</th>;
             })}
           </tr>
         </thead>
         <tbody className={bodyClass}>
-          {props.rows.map((item: any) => {
-            return <tr key={item.key}>{props.render && props.render(item)}</tr>;
+          {rows.map((item: any) => {
+            return <tr key={item.key}>{render && render(item)}</tr>;
           })}
-          {getLoadingTemplate(props)}
+          {loadingTemplate}
         </tbody>
       </table>
     </div>
